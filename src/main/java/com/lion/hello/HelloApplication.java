d097e82a00dd4f7c6edcf5d4aa3408a0bf43b8f3
joonlee <96dalee@gmail.com>
2025-06-25 15:26:50 +0900

    hellocontroller

--- src/main/java/com/lion/hello/HelloApplication.java
+++ src/main/java/com/lion/hello/HelloApplication.java
@@ -1,13 +1,12 @@
-package com.lion.hello;
+package com.lion.hello.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
-public class HelloApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
+@Controller
+public class HelloController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
 	}
-
 }
